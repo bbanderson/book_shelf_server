3e2ea1ad2b85d6b96b134332b823cf8450442524
bbanderson <tombyun@naver.com>
2021-08-12 22:57:58 +0900

    Add POST route for token

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("hello"));
+app.post("/v1/user", (req, res) =>
+  res.json({
+    token: "123",
+  })
+);
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
